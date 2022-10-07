877ad719294bad09785b2edffb68924a32583f81
Boring Wozniak <52351871+boring-wozniak@users.noreply.github.com>
2022-10-07 21:25:28 +0200

    implemented copy button

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-const input = document.getElementById('input');
-const button = document.getElementById('button');
+const inputField = document.getElementById('input');
+const copyButton = document.getElementById('copy');
 
-const generateUuid4 = () =>
-    input.value = crypto.randomUUID();
-
-generateUuid4();
-button.onclick = generateUuid4;
+copyButton.addEventListener('click', () =>
+    navigator.clipboard.writeText(inputField.value)
+);
